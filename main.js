2d1e4a4fc6b6db39a859d08287a62b81d0ad8a6b
Iakov Mishchenko <iakov@kamilabs.com>
2018-07-20 19:46:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,4 @@ const PORT = process.env.PORT || 5000
 
 express()
     .use(express.static(path.join(__dirname, 'static')))
-    .get('/', (req, res) => res.render('pages/index'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
